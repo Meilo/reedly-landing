c969a58a62b7db82e3a99f7b65518f9f773e594b
Meilo <ludovic.elice@gmail.com>
2026-03-06 09:11:18 +0100

    chore(vercel): setup vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   // 'hybrid' = static pages by default, but allows server endpoints (like our contact API)
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  // To deploy on Vercel, swap the adapter:
-  //   import vercel from '@astrojs/vercel/serverless'
-  //   adapter: vercel()
+  adapter: vercel(),
 });
